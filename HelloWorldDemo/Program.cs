0dc9e51f5ffabc4637996c1c5c7c93e55bc72302
Ashish Kumar <ashishraj916263@gmail.com>
2023-08-07 23:38:39 +0530

    Initial Commit

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Namespace Declaration
+namespace HelloWorldDemo
+{
+    // Main class declaration
+    class Program
+    {
+        // Main method, the entry point of program
+        static void Main(string[] args)
+        {
+            // Print "Hello World!" to the console 
+            Console.WriteLine("Hello World!");
+
+            //Wait for the user to press a key before closing the window
+            Console.ReadKey();
+        }
+    }
+}
